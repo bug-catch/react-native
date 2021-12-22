b5d467ad788a995290cf1d03f42f5705433ff8b1
Harry Merritt <harry@merritt.es>
2021-12-22 21:08:01 +0000

    fix old event route and remove browser error detection

--- src/bugcatch.js
+++ src/bugcatch.js
@@ -23,53 +23,15 @@ const setOptions = (userOptions) => {
  * Handle error events
  * @param {object} error event object
  */
-const onError = (evt) => {
+const onError = (evt, isFatal) => {
     // Collect error data from event
-    const data = {};
-
-    // Detect error event
-    // separate error and unhandledrejection
-    if (evt.error) {
-        // Error event
-        data.type = evt.type;
-        data.message = evt.message;
-        data.filename = evt.filename;
-        data.line = evt.lineno || -1;
-        data.column = evt.colno || -1;
-        data.error = {
-            name: evt.error.name,
-            message: evt.error.message,
-            stack: evt.error.stack,
-        };
-    } else {
-        // Promise rejection event
-        data.type = evt.type;
-        data.message = evt.reason.message;
-        data.filename = "";
-        data.line = -1;
-        data.column = -1;
-        data.error = {
-            name: evt.reason.name,
-            message: evt.reason.message,
-            stack: evt.reason.stack,
-        };
-
-        // Extract line and column numbers
-        // from stack trace
-        const stackLinePosition = (/:[0-9]+:[0-9]+/.exec(evt.reason.stack) || [
-            "",
-        ])[0].split(":");
-
-        if (stackLinePosition.length === 3) {
-            data.line = Number(stackLinePosition[1]);
-            data.column = Number(stackLinePosition[2]);
-        }
-    }
+    const data = {
+        evt,
+        isFatal,
+    };
 
     // Send incident data to server
     post(`${options.base_url}/catch/event`, newEvent("error", data, options));
-
-    return true;
 };
 
 /**
@@ -85,7 +47,7 @@ export const recordEvent = (name, data, userOptions) => {
         console.log(`[Bug Catch] Event: ${name}`, { name, data });
 
     // Send incident data to server
-    post(`${options.base_url}/event`, newEvent(name, data, options));
+    post(`${options.base_url}/catch/event`, newEvent(name, data, options));
 };
 
 /**
@@ -101,6 +63,8 @@ export const init = (userOptions) => {
         // You do stuff like show an error dialog
         // or hit google analytics to track crashes
         // or hit a custom api to inform the dev team.
-        console.log(error, isFatal);
+        if (options.logEvents) console.log(error, isFatal);
+
+        onError(error, isFatal);
     });
 };
