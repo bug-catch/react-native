6885b80209e04e6510b68960b639b03c6a1b0c9a
hmerritt <harry@mrrtt.me>
2023-08-07 01:39:49 +0100

    fix: react-native-web module resolution error

---
+++ web.js
@@ -0,0 +1,6 @@
+/**
+ * Fix for module `react-native-device-info` having error: `Unable to resolve "../web" from "node_modules/@bug-catch/react-native/dist/index.umd.js"`
+ * 
+ * This empty file should mute the error.
+ */
+export default {};
