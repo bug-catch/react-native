95f7a479ece708904f61f56b89e8f5a67021e5a1
hmerritt <harry@mrrtt.me>
2023-08-07 13:59:19 +0100

    fix: skip bundle for react-native only typescript compile

--- web.js
+++
@@ -1,6 +0,0 @@
-/**
- * Fix for module `react-native-device-info` having error: `Unable to resolve "../web" from "node_modules/@bug-catch/react-native/dist/index.umd.js"`
- * 
- * This empty file should mute the error.
- */
-export default {};
